0a9df831d13bc8b8b93d5d317acfc7dc9e44fb64
Astral0 <astral0@protonmail.com>
2019-07-17 23:44:50 +0200

    corrections

---
+++ templates/lxc-nextdom/etc/lxc-nextdom/bin/openvpn-server/make-client-config.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# First argument: Client identifier
+
+if [ "x$1" == "x" ]
+then
+  echo Need an argument
+  exit 1
+fi
+
+cd ~/openvpn-ca
+source vars
+./build-key ${1}
+
+
+KEY_DIR=~/openvpn-ca/keys
+OUTPUT_DIR=~/client-configs/files
+BASE_CONFIG=~/client-configs/base.conf
+
+cat ${BASE_CONFIG} \
+    <(echo -e '<ca>') \
+    ${KEY_DIR}/ca.crt \
+    <(echo -e '</ca>\n<cert>') \
+    ${KEY_DIR}/${1}.crt \
+    <(echo -e '</cert>\n<key>') \
+    ${KEY_DIR}/${1}.key \
+    <(echo -e '</key>\n<tls-auth>') \
+    ${KEY_DIR}/ta.key \
+    <(echo -e '</tls-auth>') \
+    > ${OUTPUT_DIR}/${1}.ovpn
+
+
