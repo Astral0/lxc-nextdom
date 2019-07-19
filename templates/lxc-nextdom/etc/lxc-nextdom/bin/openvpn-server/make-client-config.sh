aa14123d55cfd956030793a80e6ee563349a7a89
Astral0 <astral0@protonmail.com>
2019-07-19 10:49:56 +0200

    openvpn evol

--- templates/lxc-nextdom/etc/lxc-nextdom/bin/openvpn-server/make-client-config.sh
+++ templates/lxc-nextdom/etc/lxc-nextdom/bin/openvpn-server/make-client-config.sh
@@ -8,14 +8,41 @@ then
   exit 1
 fi
 
-cd ~/openvpn-ca
+# Params
+openvpn_root="/root/vpn"
+openvpn_ca=${openvpn_root}/openvpn-ca
+clients=${openvpn_root}/client-configs
+install_files="/root/install_files"
+
+# Tests
+if [ ! -d ${openvpn_ca} ] ; then
+    echo "Error ! Directory Server Certificates are not present!"
+    exit 1
+fi
+#
+mkdir -p ${clients}
+if [ ! -f "${clients}/base.conf" ] ; then
+    if [ -f "${install_files}/base.conf" ] ; then
+        cp -ax ${install_files}/base.conf ${clients}/base.conf
+    else
+        echo "Error ! Missing base.conf files !"
+    fi
+fi
+
+
+# Generate client key
+cd ${openvpn_ca}
 source vars
-./build-key ${1}
+#./build-key ${1}
+export EASY_RSA="${EASY_RSA:-.}"
+"$EASY_RSA/pkitool" ${1}
 
 
-KEY_DIR=~/openvpn-ca/keys
-OUTPUT_DIR=~/client-configs/files
-BASE_CONFIG=~/client-configs/base.conf
+
+KEY_DIR=${openvpn_ca}/keys
+OUTPUT_DIR=${clients}/files
+BASE_CONFIG=${clients}/base.conf
+mkdir -p ${OUTPUT_DIR}
 
 cat ${BASE_CONFIG} \
     <(echo -e '<ca>') \
@@ -29,4 +56,4 @@ cat ${BASE_CONFIG} \
     <(echo -e '</tls-auth>') \
     > ${OUTPUT_DIR}/${1}.ovpn
 
-
+echo "Client key written : ${OUTPUT_DIR}/${1}.ovpn"
