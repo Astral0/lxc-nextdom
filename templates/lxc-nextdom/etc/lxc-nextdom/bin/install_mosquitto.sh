5069ca739a65d17aa51e4140d40cbb141a06b43a
Astral0 <astral0@protonmail.com>
2020-05-09 22:51:40 +0200

    Add dialog boxes for Mosquitto to be compatible with Proxmox

--- templates/lxc-nextdom/etc/lxc-nextdom/bin/install_mosquitto.sh
+++ templates/lxc-nextdom/etc/lxc-nextdom/bin/install_mosquitto.sh
@@ -3,36 +3,90 @@
 #arg1=$1
 #arg2=$2
 
+
+# parameters
+MQTT_USER=
+MQTT_PASS=
+
 DEBIAN_FRONTEND=noninteractive
 export DEBIAN_FRONTEND
 
 echo " >>>> Installation de Mosquitto MQTT broker <<<<"
 
-# Install
+# Prerequisites
 apt update
 apt upgrade -y
-apt install -y software-properties-common gnupg wget ca-certificates hostname
+apt install -y  whiptail
+
+
+# Check TERM
+if [ -z ${TERM} ] ; then
+    export TERM=ansi
+else
+    set +e
+    isscreen=$(echo $TERM | grep "screen")
+    if [ ! -z ${isscreen} ] ; then
+        export TERM=ansi
+    fi
+    set -e
+fi
+#echo $TERM
+#export TERM=vt100
+
+
+# Setup login and password
+if [ -z ${MQTT_USER} ] ; then
+    MQTT_USER=$(whiptail --title "Input" --inputbox "MQTT User" 10 60 user 3>&1 1>&2 2>&3)
+    exitstatus=$? && if [ ! $exitstatus = 0 ]; then MQTT_USER=User ; fi
+fi
+
+if [ -z ${MQTT_PASS} ] ; then
+    pass=$(date +”%N” | md5sum | head -c 8 ; echo)
+    MQTT_PASS=$(whiptail --title "Input" --passwordbox "MQTT Password" 10 60 3>&1 1>&2 2>&3)
+    exitstatus=$? && if [ ! $exitstatus = 0 ]; then MQTT_PASS=${pass} ; fi
+
+    if [ -z $MQTT_PASS ]; then
+        MQTT_PASS=${pass}
+    fi
+fi
+
+
+# Prerequisites
+apt update
+apt upgrade -y
+apt install -y software-properties-common gnupg wget ca-certificates hostname whiptail apt-transport-https
+
+
+# Install
 wget -O - http://repo.mosquitto.org/debian/mosquitto-repo.gpg.key | apt-key add -
 
+if [ -d /etc/apt/sources.list.d ]; then 
+    APT_DIR=/etc/apt/sources.list.d 
+elif [ -d /etc/apt//etc/apt/sources/list.d ]; then
+    APT_DIR=/etc/apt//etc/apt/sources/list.d
+else
+    echo "Error!"
+    exit 1
+fi
+
 source /etc/os-release
-test $VERSION_ID = "7" && wget -O /etc/apt/sources/list.d/mosquitto-wheezy.list http://repo.mosquitto.org/debian/mosquitto-wheezy.list
-test $VERSION_ID = "8" && wget -O /etc/apt/sources/list.d/mosquitto-jessie.list http://repo.mosquitto.org/debian/mosquitto-jessie.list
-test $VERSION_ID = "9" && wget -O /etc/apt/sources/list.d/mosquitto-stretch.list http://repo.mosquitto.org/debian/mosquitto-stretch.list
-test $VERSION_ID = "10" && wget -O /etc/apt/sources/list.d/mosquitto-buster.list http://repo.mosquitto.org/debian/mosquitto-buster.list
+test $VERSION_ID = "7" && wget -O ${APT_DIR}/mosquitto-wheezy.list http://repo.mosquitto.org/debian/mosquitto-wheezy.list
+test $VERSION_ID = "8" && wget -O ${APT_DIR}/mosquitto-jessie.list http://repo.mosquitto.org/debian/mosquitto-jessie.list
+test $VERSION_ID = "9" && wget -O ${APT_DIR}/mosquitto-stretch.list http://repo.mosquitto.org/debian/mosquitto-stretch.list
+test $VERSION_ID = "10" && wget -O ${APT_DIR}/mosquitto-buster.list http://repo.mosquitto.org/debian/mosquitto-buster.list
 
 apt update
 apt install -y mosquitto
 
 
 # Generate a password for Mosquitto
-pass=$(date +”%N” | md5sum | head -c 8 ; echo)
 passfile="/etc/mosquitto/passwordfile"
 touch ${passfile}
-mosquitto_passwd -b ${passfile} user ${pass}
+mosquitto_passwd -b ${passfile} user ${MQTT_USER}
 
 touch /root/mosquitto_passwd
 chmod 600 /root/mosquitto_passwd
-echo "user ${pass}" >>/root/mosquitto_passwd
+echo "${MQTT_USER} ${MQTT_PASS}" >>/root/mosquitto_passwd
 
 
 cat <<EOF >> /etc/mosquitto/mosquitto.conf
@@ -64,4 +118,4 @@ service mosquitto restart
 # Show IP
 ip=$(hostname -I)
 echo " >>>>  Please open http://${ip} <<<<"
-echo " >>>> Mosquitto user/pass is : user/${pass} ! You can change it in file : ${passfile} <<<<"
+echo " >>>> Mosquitto user/pass is : ${MQTT_USER}/${MQTT_PASS} ! You can change it in file : ${passfile} <<<<"
